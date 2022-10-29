dfd29d6485aaf342289673fd1cca29f39f894f9b
James <jamesrobertsongroves@gmail.com>
2022-10-29 08:41:06 +0100

    docs

--- mix.exs
+++ mix.exs
@@ -6,6 +6,8 @@ defmodule Issues.MixProject do
       app: :issues,
       escript: escript_config(),
       version: "0.1.0",
+      name: "Issue Table",
+      source_url: "https://github.com/JimmyMcKegger/issue-table",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -21,7 +23,9 @@ defmodule Issues.MixProject do
   defp deps do
     [
       {:httpoison, "~> 1.8"},
-      {:poison, "~> 5.0"}
+      {:poison, "~> 5.0"},
+      {:ex_doc, "~> 0.29.0"},
+      {:earmark, "~> 1.4"}
     ]
   end
 
